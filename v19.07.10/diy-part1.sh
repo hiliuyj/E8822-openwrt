3c96284bca60940d27107686b36ca6bdc1a12d0e
liuyj <hiliuyj@163.com>
2026-03-27 16:30:00 +0800

    add 原厂 drv

--- v19.07.10/diy-part1.sh
+++ v19.07.10/diy-part1.sh
@@ -16,3 +16,9 @@
 # Add a feed source
 #echo 'src-git helloworld https://github.com/fw876/helloworld' >>feeds.conf.default
 #echo 'src-git passwall https://github.com/xiaorouji/openwrt-passwall' >>feeds.conf.default
+
+echo 'src-git mtk https://github.com/Nossiac/mtk-openwrt-feeds' >>feeds.conf.default
+
+
+
+
